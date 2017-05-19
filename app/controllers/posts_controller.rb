f871d4b54c2a73d4106382361ff86c147b943f01
Salina <salina.sallu@outlook.com>
2017-05-19 16:29:22 +0545

    filters

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	before_action :authenticate_user!, only: [:new , :create]
+	before_action :is_owner?, only: [:edit, :update]
 	def create
   		@post = current_user.posts.create(post_params)
   		if @post.valid?
@@ -37,5 +38,9 @@ class PostsController < ApplicationController
 		params.require(:post).permit(:user_id, :photo, :description)
 	end
 
+	def is_owner?
+		redirect_to root_path if Post.find(params[:id]).user != current_user
+		end
+	end
 
 end
