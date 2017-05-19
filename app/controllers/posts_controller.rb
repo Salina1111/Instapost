a9b5d258471933ac9d92a4daaf1227ec58568d99
Salina <salina.sallu@outlook.com>
2017-05-19 15:08:36 +0545

    post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,12 @@
 class PostsController < ApplicationController
 	def create
-  @post = Post.find(params[:id])
-  @post.create(post_params)
-  redirect_to root_path
-end
+  		@post = Post.find(params[:id])
+  		@post.create(post_params)
+  		redirect_to root_path
+	end
+
+	def new
+		@post = Post.new
+	end
 
 end
