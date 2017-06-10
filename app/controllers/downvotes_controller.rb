2d87ea7472a41e4bd0c164293c3fbe74c725a6ca
Salina <salina.sallu@outlook.com>
2017-06-10 21:55:50 +0545

    AJAX

--- app/controllers/downvotes_controller.rb
+++ app/controllers/downvotes_controller.rb
@@ -1,14 +1,13 @@
 class DownvotesController < ApplicationController
-	before_action :authentication_user!
+	before_action :authenticate_user!
 
 	def create
-		@post = Post.find(params[:post_id])
+		@post=Post.find(params[:post_id])
 		@post.downvote_from current_user
-		redirect_to posts_path
-
+		
 		respond_to do |format|
-			format.html {}
+			format.html {redirect_to posts_path}
 			format.js {}
-		end
+		end	
 	end
 end
